2d623d0cddb1f8e5d03444fffe00f78b6c93994f
Andrey Tikhonov <andrey.tikhonov.1974@gmail.com>
2020-11-24 20:23:24 +0200

    Changed names of handlers

--- run_service.py
+++ run_service.py
@@ -21,14 +21,14 @@ async def root():
     return {"message": "API for providing historical stocks and news data. You can find the API description on:  http://host:port/docs or http://host:port/redoc"}
 
 @app.get('/historical/{company}')
-async def wiki(company:str, start_date: Optional[date] = Query(None), end_date: Optional[date] = Query(None)):
+async def historical(company:str, start_date: Optional[date] = Query(None), end_date: Optional[date] = Query(None)):
     print(company, start_date, end_date)
     query = get_historical_data(company, start_date, end_date)
     print(query)
     return await connection.get_db().fetch_all(query)
 
 @app.get('/news/{company}')
-async def wiki(company:str, start_date: Optional[date] = Query(None), end_date: Optional[date] = Query(None)):
+async def news(company:str, start_date: Optional[date] = Query(None), end_date: Optional[date] = Query(None)):
     query = get_news_data(company, start_date, end_date)
     return await connection.get_db().fetch_all(query)
 
